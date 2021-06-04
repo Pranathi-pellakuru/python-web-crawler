3b2488439c2e9529cd4c896162171d5a0bf4a33d
Pranathi-pellakuru <84898399+Pranathi-pellakuru@users.noreply.github.com>
2021-06-04 22:28:10 +0530

    Update crawler.py

--- crawler.py
+++ crawler.py
@@ -7,28 +7,28 @@ def cycle(records):
     linkspresent =links_to_be_crawled(records)
     if len(linkspresent)==0:
         return True
-        #returns true when there are no links that should be crawled
+                                                                                   #returns true when there are no links that should be crawled
     else:
-        #multithreading using threadpoolexecutor with maximum of 5 threads
+                                                                                   #multithreading using threadpoolexecutor with maximum of 5 threads
         try:
             with concurrent.futures.ThreadPoolExecutor(max_workers=max_threads) as executor:
                 executor.map(crawl,linkspresent)     
                  
             time.sleep(5)
         
-        #after every cycle the process sleeps for 5 secs
+                                                                                   #after every cycle the process sleeps for 5 secs
         except Exception as e:
             print(e)
         return False
 if __name__=="__main__":
     records=connect_database(database_connection,db_name,collection_name)
-    #connects to the database
+                                                                                   #connects to the database
     add_rooturl(rooturl,records)
     while True:
         count=countdocs(records)
         if count<limit:
             progress=cycle(records)
-            #cycle returns true when there are no links that are to be crawled
+                                                                                  #cycle returns true when there are no links that are to be crawled
             if progress:
                 print("all links are crawled")
                 break
